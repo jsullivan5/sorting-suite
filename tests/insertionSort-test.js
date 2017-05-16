9a37ff502fc21fd092544e0bd9262786529cdde8
James Sullivan <jamesfsullivan5@gmail.com>
2017-05-16 15:05:31 -0600

    Add insertion sort and tests.

--- tests/insertionSort-test.js
+++ tests/insertionSort-test.js
@@ -1,17 +1,17 @@
 import { expect } from 'chai';
-import bubbleSort from '../scripts/bubbleSort.js';
+import insertionSort from '../scripts/insertionSort.js';
 import genRanNum from '../scripts/genRanNum.js';
 import charGen from '../scripts/charGen.js';
 
-describe('Bubble sort', () => {
+describe('Insertion Sort', () => {
 
   it('should be a function', () => {
-    expect(bubbleSort).to.be.instanceof(Function);
+    expect(insertionSort).to.be.instanceof(Function);
   });
 
   it('should sort an array of positive numbers', () => {
     let numbers = [ 5, 3, 2, 1 ];
-    let sorted = bubbleSort(numbers);
+    let sorted = insertionSort(numbers);
 
     expect(sorted).to.deep.equal([ 1, 2, 3, 5 ]);
   });
@@ -20,7 +20,7 @@ describe('Bubble sort', () => {
     let negNumArray = [ -1, -100, -4, -28 ];
     let sortedNegArray = [ -100, -28, -4, -1 ];
 
-    expect(bubbleSort(negNumArray)).to.deep.equal(sortedNegArray);
+    expect(insertionSort(negNumArray)).to.deep.equal(sortedNegArray);
   })
 
   it('should sort positive and negative numbers', () => {
@@ -28,7 +28,7 @@ describe('Bubble sort', () => {
     let sortedPosNegArray = [ -50, -6, 4, 18];
 
     expect(posNegArray).to.not.equal(sortedPosNegArray);
-    expect(bubbleSort(posNegArray)).to.deep.equal(sortedPosNegArray);
+    expect(insertionSort(posNegArray)).to.deep.equal(sortedPosNegArray);
   })
 
   it('should sort lowercase letters', () => {
@@ -36,14 +36,14 @@ describe('Bubble sort', () => {
     let sortedLetters = [ 'a', 'd', 'j', 'r', 'y' ];
 
     expect(letters).to.not.equal(sortedLetters);
-    expect(bubbleSort(letters)).to.deep.equal(sortedLetters)
+    expect(insertionSort(letters)).to.deep.equal(sortedLetters)
   })
 
   it('should sort large arrays of numbers', () => {
     let randomArray = genRanNum(1000);
     let compSorted = Array.from(randomArray).sort( (a, b) => a - b);
 
-    expect(bubbleSort(randomArray))
+    expect(insertionSort(randomArray))
     .to.deep.equal(compSorted)
   })
 
@@ -52,6 +52,6 @@ describe('Bubble sort', () => {
     let compSorted = Array.from(ranCharArray).sort();
 
     expect(ranCharArray).to.not.deep.equal(compSorted);
-    expect(bubbleSort(ranCharArray)).to.deep.equal(compSorted);
+    expect(insertionSort(ranCharArray)).to.deep.equal(compSorted);
   })
 })
