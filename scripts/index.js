f127ece83ca33f014a864c55790cde5a294aefd5
James Sullivan <jamesfsullivan5@gmail.com>
2017-05-18 10:09:54 -0600

    Add index.js file.

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import bubbleSort from '../scripts/bubbleSort.js';
+import { mergeSort } from '../scripts/mergeSort.js';
+import insertionSort from '../scripts/insertionSort.js';
+import quickSort from '../scripts/quickSort.js';
+
+export { bubbleSort, mergeSort, insertionSort, quickSort }
